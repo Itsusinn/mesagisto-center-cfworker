d041848c7d300eb505090c7d499ef6054e66ad1f
Itsusinn <itsusinn@meowcat.org>
2023-04-18 17:11:04 +0800

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use futures_util::StreamExt;
 use serde::{Deserialize, Serialize};
 use worker::*;
 
@@ -19,8 +20,43 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
       let value = Account { id: 3 };
       ciborium::ser::into_writer(&value, &mut writer).unwrap();
       let msgs = ctx.kv("KVTEST")?;
-      msgs.put("test", "test").unwrap().execute().await.unwrap();
+      msgs.put_bytes("test", &writer).unwrap().execute().await.unwrap();
       Response::from_html(hex::encode(&writer))
+    })
+    .get("/", |_, ctx| {
+      // Accept / handle a websocket connection
+      let pair = WebSocketPair::new()?;
+      let server = pair.server;
+      server.accept()?;
+
+      let some_namespace_kv = ctx.kv("KVTEST")?;
+
+      wasm_bindgen_futures::spawn_local(async move {
+        let mut event_stream = server.events().expect("could not open stream");
+
+        while let Some(event) = event_stream.next().await {
+          match event.expect("received error in websocket") {
+            WebsocketEvent::Message(msg) => {
+              if let Some(text) = msg.text() {
+                server.send_with_str(text).expect("could not relay text");
+              }
+            }
+            WebsocketEvent::Close(_) => {
+              // Sets a key in a test KV so the integration tests can query if we
+              // actually got the close event. We can't use the shared dat a for this
+              // because miniflare resets that every request.
+              some_namespace_kv
+                .put("got-close-event", "true")
+                .unwrap()
+                .execute()
+                .await
+                .unwrap();
+            }
+          }
+        }
+      });
+
+      Response::from_websocket(pair.client)
     })
     .run(req, env)
     .await
